6286c73e6cf4b5f534541740403bd0366c8958c1
avandendaele <alexis.vandendaele@gmail.com>
2015-12-31 19:17:47 +0100

    Updated schema

--- domain/transfer.js
+++ domain/transfer.js
@@ -5,13 +5,12 @@ const transactionStates = ['OPEN', 'ASSIGNED', 'PAID', 'CLOSED'];
 const TransferSchema = new mongoose.Schema({
   from: { type: String, required: false },
   to: { type: String, required: true },
+  toAka: { type: String, required: true },
   amount: { type: Number, min: 0, required: true },
   currency: { type: String, required: true },
   status: { type: String, required: true, enum: transactionStates },
   communication: { type: String, required: false },
-  date: {
-    due: { type: Date, required: true}
-  }
+  dueDate: { type: String, required: true}
 }, { _id: true, id: true })
 
 export default mongoose.model('Transfer', TransferSchema);
--- domain/user.js
+++ domain/user.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
-  lastName: { type: String, required: true }
+  lastName: { type: String, required: true },
+  accounts: { type: [String] }
 }, { _id: true, id: true })
 
 export default mongoose.model('User', UserSchema);
